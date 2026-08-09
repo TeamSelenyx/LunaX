5febafd234cb545becdc04fa002f38c8b5ed7dd1
NeroLuna <nerolunakr@gmail.com>
2026-08-09 20:00:07 +0900

    🚀 Luna X - 1.1.0 start

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,9 +31,9 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "Luna X";
-	public const LUNA_X_VERSION = "1.0.0";
+	public const LUNA_X_VERSION = "1.1.0";
 	public const BASE_VERSION = "5.44.4";
-	public const IS_DEVELOPMENT_BUILD = false;
+	public const IS_DEVELOPMENT_BUILD = true;
 	public const BUILD_CHANNEL = "stable";
 	public const GITHUB_URL = "https://github.com/TeamSelenyx/LunaX";
 
