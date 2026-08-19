d471534e0b6e8104eab0435724c31dcf0fe6cccb
NeroLuna <nerolunakr@gmail.com>
2026-08-20 00:11:27 +0900

    🚀 Luna X에 필요한 도구들 업데이트

--- vendor-install.ps1
+++ vendor-install.ps1
@@ -1,3 +1,5 @@
-$Host.UI.RawUI.WindowTitle = "Vendor Install"
+﻿$Host.UI.RawUI.WindowTitle = "LunaX - Vendor Install"
+echo "서버 구동에 필요한 vendor 파일들을 설치하는 중..."
+echo " "
 ./bin/php/php.exe ./bin/composer.phar install
 pause
